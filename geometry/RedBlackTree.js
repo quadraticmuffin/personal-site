10f7e3657b7d45e3efac7efe15d20f949c0a79ea
jett <jettwang28@gmail.com>
2024-12-29 02:04:45 -0500

    successor queries working on Red-Black Tree. not persistent yet, currently rebuilding tree for every query.

--- geometry/RedBlackTree.js
+++ geometry/RedBlackTree.js
@@ -16,16 +16,22 @@ class Node {
         this.left = left;
         this.right = right;
     }
+    getValue(x) {
+        if (this.data === null) {
+            return null;
+        }
+        return this.data.getY(x);
+    }
 }
 
 class RedBlackTree {
-    constructor(key, color, left, right, parent) {
-        this.key = key;          // Key for the node
-        this.color = color;      // Color of the node: 'red' or 'black'
+    constructor() {
+        // this.key = key;          // Key for the node
+        // this.color = color;      // Color of the node: 'red' or 'black'
 
-        this.left = left;        // Left child
-        this.right = right;       // Right child
-        this.parent = parent;      // Parent
+        // this.left = left;        // Left child
+        // this.right = right;       // Right child
+        // this.parent = parent;      // Parent
 
         this.back_left = null;
         this.back_right = null;
@@ -36,6 +42,8 @@ class RedBlackTree {
 
         this.TNULL = new Node(null, 0); // Sentinel node
         this.root = this.TNULL;
+
+        this.x = 0;
     }
 
     read(field, version=undefined) {
@@ -75,6 +83,22 @@ class RedBlackTree {
         }
     }
 
+    preorder() {
+        this.preOrderHelper(this.root);
+    }
+
+    inorder() {
+        this.inOrderHelper(this.root);
+    }
+
+    postorder() {
+        this.postOrderHelper(this.root);
+    }
+
+    search(key) {
+        return this.searchTreeHelper(this.root, key);
+    }
+
     // Preorder traversal
     preOrderHelper(node) {
         if (node !== this.TNULL) {
@@ -107,10 +131,97 @@ class RedBlackTree {
         if (node === this.TNULL || key === node.data) {
             return node;
         }
-        if (key < node.data) {
+        if (key < node.getValue(this.x)) {
             return this.searchTreeHelper(node.left, key);
         }
         return this.searchTreeHelper(node.right, key);
+    }
+
+    minimum(node) {
+        while (node.left !== this.TNULL) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    successorHelper(node, key) {
+        if (node === this.TNULL) {
+            return null;
+        }
+        // console.log(`successorHelper: ${node.data.x}, ${node.data.y}, ${node.data.other.x}, ${node.data.other.y}`);
+        // console.log(`successorHelper: ${key < node.getValue(this.x)}`);
+        if (key < node.getValue(this.x)) {
+            const left_succ = this.successorHelper(node.left, key);
+            if (left_succ !== null) {
+                // console.log(`comparing ${node.getValue(this.x)} and ${left_succ.getY(this.x)}`);
+                return node.getValue(this.x) < left_succ.getY(this.x) ? node.data : left_succ;
+            }
+            // console.log(`returning ${node.data.x}, ${node.data.y}, ${node.data.other.x}, ${node.data.other.y}`);
+            return node.data;
+        }
+        return this.successorHelper(node.right, key);
+    }
+
+    successor(key) {
+        console.log(`finding successor of ${key}`);
+        return this.successorHelper(this.root, key);
+    }
+
+    rbTransplant(u, v) {
+        if (u.parent === null) {
+            this.root = v;
+        } else if (u === u.parent.left) {
+            u.parent.left = v;
+        } else {
+            u.parent.right = v;
+        }
+        v.parent = u.parent;
+    }
+
+
+    deleteNodeHelper(node, key) {
+        let z = this.TNULL;
+        let x, y;
+        while (node !== this.TNULL) {
+            if (node.data === key) {
+                z = node;
+            }
+            if (node.getValue(this.x) <= key) {
+                node = node.right;
+            } else {
+                node = node.left;
+            }
+        }
+        if (z === this.TNULL) {
+            return;
+        }
+        y = z;
+        let yOriginalColor = y.color;
+        if (z.left === this.TNULL) {
+            x = z.right;
+            this.rbTransplant(z, z.right);
+        } else if (z.right === this.TNULL) {
+            x = z.left;
+            this.rbTransplant(z, z.left);
+        } else {
+            y = this.minimum(z.right);
+            yOriginalColor = y.color;
+            x = y.right;
+            if (y.parent === z) {
+                x.parent = y;
+            } else {
+                this.rbTransplant(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+            this.rbTransplant(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            y.color = z.color;
+        }
+        if (yOriginalColor === 0) {
+            this.fixDelete(x);
+        }
     }
 
     // Balance the tree after deletion
@@ -169,6 +280,10 @@ class RedBlackTree {
         x.color = 0;
     }
 
+    deleteNode(key) {
+        this.deleteNodeHelper(this.root, key);
+    }
+
     // Rotate left
     leftRotate(x) {
         let y = x.right;
@@ -216,7 +331,7 @@ class RedBlackTree {
 
         while (x !== this.TNULL) {
             y = x;
-            if (node.data < x.data) {
+            if (node.getValue(this.x) < x.getValue(this.x)) {
                 x = x.left;
             } else {
                 x = x.right;
@@ -226,7 +341,7 @@ class RedBlackTree {
         node.parent = y;
         if (y === null) {
             this.root = node;
-        } else if (node.data < y.data) {
+        } else if (node.getValue(this.x) < y.getValue(this.x)) {
             y.left = node;
         } else {
             y.right = node;
@@ -301,21 +416,9 @@ class RedBlackTree {
         this.printHelper(this.root, "", true);
     }
 }
-    
-
-    
-// Hard-coded Red-Black Tree
-// Example Usage
-const exampleRedBlackTree = new RedBlackTree();
-exampleRedBlackTree.insert(55);
-exampleRedBlackTree.insert(40);
-exampleRedBlackTree.insert(65);
-exampleRedBlackTree.insert(60);
-exampleRedBlackTree.insert(75);
-exampleRedBlackTree.insert(57);
 
 // Function to visualize the tree on the canvas
-function drawTree(canvasId, tree) {
+function drawTree(canvasId, tree, highlight) {
     const canvas = document.getElementById(canvasId);
     const ctx = canvas.getContext('2d');
 
@@ -324,7 +427,12 @@ function drawTree(canvasId, tree) {
     function drawTreeInner(node, x, y, prev_x) {
         ctx.fillStyle = (node.color === 1 ? "RED" : "BLACK");
         const dx = Math.abs((x - prev_x) / 2);
-        ctx.fillText(node.data, x, y);
+        if (node !== tree.TNULL) {
+            if (highlight && node.data == highlight) {
+                ctx.fillStyle = ("GREEN");
+            }
+            ctx.fillText(node.data.valueOf(), x, y);
+        }
         if (node.left) {
             drawTreeInner(node.left, x - dx, y + 30, x);
         }
@@ -338,4 +446,4 @@ function drawTree(canvasId, tree) {
 
 // Exporting the RedBlackTree class
 export default RedBlackTree;
-export { drawTree, exampleRedBlackTree}; 
+export { drawTree }; 
